9301763bff0f34616cce7307ec087b8ff60e6d9a
kar_lef <kar_lef@5072B12E.static.ziggozakelijk.nl>
2017-05-24 19:05:20 +0200

    Ex5

---
+++ module5-solution/src/public/form/formvalidation.module.js
@@ -0,0 +1,15 @@
+(function{
+	'use strict';
+	angular.module('FormValidation',[]);
+
+	angular.module('FormValidation')
+	.controller('RegistrationController', RegistrationController);
+
+	function RegistrationController() {
+	  var reg = this;
+
+	  reg.submit = function () {
+	    reg.completed = true;
+	  };
+	}
+})();
