05c4d4ea1d2181f84ff4cd77044216ac3a50a11b
kar_lef <kar_lef@5072B12E.static.ziggozakelijk.nl>
2017-05-25 21:07:59 +0200

    Ex5

--- module5-solution/src/public/formvalidation.module.js
+++ module5-solution/src/public/formvalidation.module.js
@@ -10,11 +10,12 @@
 	  var reg = this;
 
 	  reg.submit = function (itemshortname) {
-	  	console.log(itemshortname);
 	  	reg.completed = true;
 	  	return $http({method: "GET",url: ("https://let369-module5.herokuapp.com/menu_items/"+ itemshortname +".json")})
 		.then(function (response) {
 			return response.data.menu_items;
+  		},function(response){
+  			console.log("error");
   		});
 	  };
 	}
